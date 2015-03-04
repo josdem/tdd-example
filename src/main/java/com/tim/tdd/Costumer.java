cd39e7f520c7a419c488cb0adffa5b4c3a001380
josdem <joseluis.delacruz@gmail.com>
2015-03-04 11:39:16 -0600

    Adding functionality to add stars and toggle newbie

--- src/main/java/com/tim/tdd/Costumer.java
+++ src/main/java/com/tim/tdd/Costumer.java
@@ -3,13 +3,19 @@ package com.tim.tdd;
 import com.tim.tdd.bean.CostumerType;
 
 public class Costumer {
+	
+	private Integer stars = 0;
 
 	public CostumerType getStatus() {
 		return CostumerType.BASIC;
 	}
 
 	public Boolean isNewbie() {
-		return true;
+		return stars==0;
+	}
+
+	public void addStar() {
+		stars++;
 	}
 
 }
--- src/test/java/com/tim/tdd/TestCostumer.java
+++ src/test/java/com/tim/tdd/TestCostumer.java
@@ -1,6 +1,7 @@
 package com.tim.tdd;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -8,17 +9,24 @@ import org.junit.Test;
 import com.tim.tdd.bean.CostumerType;
 
 public class TestCostumer {
-	
+
 	private Costumer costumer = new Costumer();
 
 	@Test
 	public void shouldKnowIfIsBasicCostumer() throws Exception {
 		assertEquals(CostumerType.BASIC, costumer.getStatus());
 	}
-	
+
 	@Test
-  public void shouldKnowIfCostumerIsNewbie() throws Exception {
-    assertTrue(costumer.isNewbie());
-  }
-	
+	public void shouldKnowIfCostumerIsNewbie() throws Exception {
+		assertTrue(costumer.isNewbie());
+	}
+
+	@Test
+	public void shouldKnowIsNotNewbie() throws Exception {
+		assertTrue(costumer.isNewbie());
+		costumer.addStar();
+		assertFalse(costumer.isNewbie());
+	}
+
 }
